6fab867fbc41c099ddadaab36d32d26bf91d4ab3
Allwyn K Geevarughese <66422903+Allwyn-K-Geevarughese@users.noreply.github.com>
2024-09-13 17:33:12 +0530

    Update SoundexTest.java

--- src/test/java/CodeTestCoverJava/SoundexTest.java
+++ src/test/java/CodeTestCoverJava/SoundexTest.java
@@ -8,78 +8,32 @@ public class SoundexTest {
 
     @Test
     public void testEmptyString() {
-        assertEquals("", Soundex.generateSoundex(""));
-    }
-
-    @Test
-    public void testNullString() {
-        assertEquals("", Soundex.generateSoundex(null));
+        assertEquals(Soundex.generateSoundex(""), "");
     }
 
     @Test
     public void testSingleCharacter() {
-        assertEquals(Soundex.generateSoundex("A"), "A000");
-    }
-
-    @Test
-    public void testVowels() {
-        assertEquals("A000", Soundex.generateSoundex("AEIOU"));
-    }
-
-    @Test
-    public void testConsonants() {
-        assertEquals("S530", Soundex.generateSoundex("Smith")); // S530
-        assertEquals("R163", Soundex.generateSoundex("Robert")); // R163
-    }
-
-    @Test
-    public void testRepeatedConsonants() {
-        assertEquals("T315", Soundex.generateSoundex("Ttfn")); // T315
-    }
-
-    @Test
-    public void testPadding() {
-        assertEquals("W623", Soundex.generateSoundex("Wright")); // W623
-        assertEquals("P620", Soundex.generateSoundex("Park"));   // Updated to P620
-    }
-
-    @Test
-    public void testMixedCase() {
-        assertEquals("R163", Soundex.generateSoundex("RoBeRt")); // R163
-    }
-
-    @Test
-    public void testHAndWHandling() {
-        assertEquals("R000", Soundex.generateSoundex("RWH")); // R000
-    }
-  @Test
-    public void testEmptyString() {
-        assertEquals(Soundex.computeSoundexCode(""), "");
-    }
-
-    @Test
-    public void testSingleCharacter() {
-        assertEquals(Soundex.computeSoundexCode("B"), "B000");
+        assertEquals(Soundex.generateSoundex("B"), "B000");
     }
 
     @Test
     public void testStringWithSpace() {
-        assertEquals(Soundex.computeSoundexCode("Doe John"), "D500");
+        assertEquals(Soundex.generateSoundex("Doe John"), "D500");
     }
 
     @Test
     public void testStringWithVowelsOnly() {
-        assertEquals(Soundex.computeSoundexCode("AEIOU"), "A000");
+        assertEquals(Soundex.generateSoundex("AEIOU"), "A000");
     }
 
     @Test
     public void testStringForSoundJ() {
-        assertEquals(Soundex.computeSoundexCode("Jenkins"), "J525");
+        assertEquals(Soundex.generateSoundex("Jenkins"), "J525");
     }
 
     @Test
     public void testStringForSoundH() {
-        assertEquals(Soundex.computeSoundexCode("Hamilton"), "H543");
+        assertEquals(Soundex.generateSoundex("Hamilton"), "H543");
     }
 
     
